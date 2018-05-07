58f20fc66b636fa12c992983ae9ba59e62569ae7
cjshinning <784319417@qq.com>
2018-05-07 21:21:34 +0800

    单组件

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      num:1,
+      title: 'imooc'
+    }
+    console.log('构造函数执行')
+    this.handleClick = this.handleClick.bind(this)
+    this.item = {react:'redux'}
+  }
+  handleClick(){
+    this.setState({
+      num: this.state.num + 1
+    })
+    // console.log('xx')
+  }
   render() {
+    // 每次渲染App bind都会执行一次
+    console.log('render执行中')
+    // const item = {react:'redux'}
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h2>App,{this.state.num}</h2>
+        <button onClick={this.handleClick.bind(this)}>Btn1</button>
+        <button onClick={()=>this.handleClick()}>Btn2</button>
+        <button onClick={this.handleClick}>Btn2</button>
+        <p>每一次都生成新的对象传递</p>
+        {/* <Demo style={{color:'red'}} name={this.item}></Demo> */}
+        {/* <Demo {...this.state}></Demo> */}
+        <Demo title={this.state.title}></Demo>
       </div>
     );
+  }
+}
+
+class Demo extends React.Component{
+  render(){
+    return <h2>I am Demo</h2>
   }
 }
 
